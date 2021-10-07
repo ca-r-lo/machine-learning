ef28795d8d0ab02b707fbed53cadb7d19b107a5d
shesh <79000021+carlo-vbs@users.noreply.github.com>
2021-10-07 18:54:57 +0800

    crated linear algebra

---
+++ linear algebra/linear.py
@@ -0,0 +1,22 @@
+
+class vector:
+    # vectors
+    # add
+    def add(vector1, vector2):
+        return (vector1[0]+vector2[0],vector1[1]+vector2[1])
+    # scaling 
+    def scale(vector, scalar):
+        return (vector[0]*scalar, vector[1]*scalar)
+
+    # iHat, jHat chu chu, ahhh basis vectors
+    # linear combinations
+    # span
+    def span():
+        pass
+
+
+
+
+
+
+
---
+++ linear algebra/test.py
@@ -0,0 +1,4 @@
+from linear import vector as vc
+
+print(vc.add([1,2],[3,-1]))
+print(vc.scale([1,2],2))
